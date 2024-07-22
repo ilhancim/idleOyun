11772dbfe2b788eeb7d98ce5fd694874b626c506
ilhancim <kilicilhan.75@gmail.com>
2024-07-22 17:59:10 +0300

    Market kısmına stok ve para göstergesi eklendi.

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-def market(para, artis, fiyatArtis):
+def market(para, artis, fiyat1, stok1):
     while True:
-        secim = int(input(f" 1-artis(+1) >{fiyatArtis} para<\n 2-***\n 3-Cik\n>-<\n "))
-        print(">-<")
+        print(f"para = {para}")
+        if stok1 >= 2:
+            secim = int(input(f" 1-artis(+1) >{fiyat1} para< ^{11-stok1}/10^\n 2-***\n 3-Cik\n>-<\n "))
+            print(">-<")
+
+        else:
+            secim = int(input(f" 1-artis x{11 - stok1}/10x\n 2-***\n 3-Cik\n>-<\n "))
+            print(">-<")
+
         if secim == 1:
-            if para >= fiyatArtis:
+            if para >= fiyat1 and stok1 >= 1:
                 artis += 1
-                para -= fiyatArtis
-                fiyatArtis = int(fiyatArtis * 1.5)
-                print(f"******\n1artis {artis} ye yukseldi!\n******")
+                stok1 -= 1
+                para -= fiyat1
+                fiyat1 = int(fiyat1 * 1.5)
+
+                print(f"******\nartis {artis} ye yukseldi!\n******")
                 continue
+
+            elif stok1 == 1:
+                print("******\nBu esyanin stogu bitti!\n******")
+                continue
+
             else:
-                print("para yetersiz!")
+                print("******\nPara yetersiz!\n******")
                 continue
 
         elif secim == 2:
@@ -23,15 +37,17 @@ def market(para, artis, fiyatArtis):
         else:
             print("Gecersiz secim!")
 
-        return para, artis, fiyatArtis
+        return para, artis, fiyat1, stok1
 
 def tikla(para, artis):
     para += artis
     return int(para)
+
 def basla():
-    para = 10
+    para = 0
     artis = 1
-    fiyatArtis = 10
+    fiyat1 = 10
+    stok1 = 10
     while True:
         print(f"para = {para}")
         secim = int(input(" 1-Tikla\n 2-Market\n>-<\n "))
@@ -40,11 +56,10 @@ def basla():
             para = tikla(para, artis)
 
         elif secim==2:
-            para, artis, fiyatArtis = market(para, artis, fiyatArtis)
+            para, artis, fiyat1, stok1 = market(para, artis, fiyat1, stok1)
 
         else:
             print("Gecersiz secim.")
-
 
 def nasilOynanir():
     print("Yakinda!")
@@ -57,9 +72,11 @@ def menu():
         if secim == 1:
             basla()
             break
+
         elif secim == 2:
             nasilOynanir()
             break
+
         elif secim == 3:
             print("Cikis yapiliyor...")
             break
@@ -67,6 +84,7 @@ def menu():
         else:
             secim = int(input("Gecerli bir numara seciniz\n>-<\n "))
             print(">-<")
+
 def main():
     menu()
 
