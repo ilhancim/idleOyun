0bbd37ab62cac408d4ebc378e86f952cb90dd01b
ilhancim <kilicilhan.75@gmail.com>
2024-08-27 16:36:44 +0300

    Fonksiyonlar güncellendi/market dışında program çalışır halde

--- fonksiyonlar.py
+++ fonksiyonlar.py
@@ -8,12 +8,13 @@ def giris(kullaniciAdi, sifre):
     for oyuncu in oyuncular:
         if kullaniciAdi == oyuncu.split("/")[0].split()[0]:
             if sifre == oyuncu.split("/")[0].split()[1]:
-                para, artis, carpan, fiyat1, fiyat2, stok1, stok2 = oyuncu.split("/")[1].split()
+                para, rebirth, prestige, artis, carpan1, carpan2, fiyat1, fiyat2, fiyat3, stok1, stok2 = oyuncu.split("/")[1].split()
                 girisDurum = 1
-                return girisDurum, kullaniciAdi, sifre, int(para), int(artis), float(carpan), int(fiyat1), int(fiyat2), int(stok1), int(stok2)
+                return girisDurum, kullaniciAdi, sifre, int(para), int(rebirth), int(prestige), int(artis), float(carpan1), int(carpan2), int(fiyat1), int(fiyat2), int(fiyat3), int(stok1), int(stok2)
                 break
 
-    return girisDurum, None, None, None, None, None, None, None, None, None
+    return girisDurum, None, None, None, None, None, None, None, None, None, None, None, None, None
+
 def kayit(kullaniciAdi, sifre):
     oku = open("Oyuncular.txt", "r")
     yaz = open("Oyuncular.txt", "a")
@@ -28,12 +29,12 @@ def kayit(kullaniciAdi, sifre):
             kayitDurum = 0
             return kayitDurum
 
-    yaz.write(f"{kullaniciAdi} {sifre}/0 10 1 100 200 10 10\n")
+    yaz.write(f"{kullaniciAdi} {sifre}/0 0 0 10 1 1 100 5000 50000 10 25\n")
     yaz.close()
 
     return kayitDurum
 
-def oyuncuGuncelle(kullaniciAdi, sifre, para, artis, carpan, fiyat1, fiyat2, stok1, stok2):
+def oyuncuGuncelle(kullaniciAdi, sifre, para, rebirth, prestige, artis, carpan1, carpan2, fiyat1, fiyat2, fiyat3, stok1, stok2):
     oku = open("Oyuncular.txt","r")
     oyuncular = []
     for oyuncu in oku:
@@ -47,92 +48,73 @@ def oyuncuGuncelle(kullaniciAdi, sifre, para, artis, carpan, fiyat1, fiyat2, sto
     ekle = open("Oyuncular.txt", "a")
     for oyuncu in oyuncular:
         if kullaniciAdi != oyuncu.split("/")[0].split()[0]:
-            ekle.write(oyuncu + "\n")
+            ekle.write(oyuncu)
 
-    ekle.write(f"{kullaniciAdi} {sifre}/{para} {artis} {carpan} {fiyat1} {fiyat2} {stok1} {stok2}\n")
+    ekle.write(f"{kullaniciAdi} {sifre}/{para} {rebirth} {prestige} {artis} {carpan1} {carpan2} {fiyat1} {fiyat2} {fiyat3} {stok1} {stok2}\n")
     ekle.close()
 
-def market(para, artis, carpan, fiyat1, fiyat2, stok1, stok2):
-    while True:
-        print(f"para = {para}")
-        if stok1 >= 2:
-            print(f" 1-artis(+1) >{fiyat1} para< ^{11-stok1}/10^")
+def guc(para, artis, fiyat1, stok1):
+    if para >= fiyat1 and stok1 >= 1:
+        artis += 10
+        stok1 -= 10
+        para -= fiyat1
+        fiyat1 = int(fiyat1 * 1.5)
 
-        else:
-            print(f" 1-artis x{11 - stok1}/10x")
+        text = f"******\nartis {artis} ye yukseldi!\n******"
 
-        if stok2 >= 2:
-            print(f" 2-carpan(+0.1) >{fiyat2} para< ^{11-stok2}/10^")
+    elif stok1 == 1:
+        text = "******\nBu esyanin stogu bitti!\n******"
 
-        else:
-            print(f" 2-carpan x{11 - stok2}/10x")
+    else:
+        text = "******\nPara yetersiz!\n******"
 
-        secim = int(input(" 3-Cik\n>-<\n "))
-        print(">-<")
+    return text, para, artis, fiyat1, stok1
 
-        if secim == 1:
-            if para >= fiyat1 and stok1 >= 1:
-                artis += 10
-                stok1 -= 1
-                para -= fiyat1
-                fiyat1 = int(fiyat1 * 1.5)
+def rebirth(para, rebirth, artis, carpan, fiyat2, stok1, stok2):
+    if para >= fiyat2 and stok2 >= 1 and stok1 == 1:
+        carpan += 0.1
+        carpan == float(f"{carpan:.1f}")
+        stok1 = 10
+        stok2 -= 1
+        artis = 10
+        para = 0
+        rebirth += 1
+        fiyat2 = int(fiyat2 * 2)
 
-                print(f"******\nartis {artis} ye yukseldi!\n******")
-                continue
+        text = f"******\nRebirth {rebirth} ye yukseldi!\n******"
 
-            elif stok1 == 1:
-                print("******\nBu esyanin stogu bitti!\n******")
-                continue
+    elif stok2 == 1:
+        text = "******\nDaha fazla Rebirth satın alamazsın! Prestige satın al!\n******"
 
-            else:
-                print("******\nPara yetersiz!\n******")
-                continue
+    elif para < fiyat2:
+        text = "******\nPara yetersiz!\n******"
 
-        elif secim == 2:
-            if para >= fiyat2 and stok2 >= 1:
-                carpan += 0.1
-                carpan == float(f"{carpan:.1f}")
-                stok2 -= 1
-                para -= fiyat2
-                fiyat2 = int(fiyat2 * 2)
+    elif stok1 > 1:
+        text = "Güc fullenmeden Rebirth satın alamazsın!"
 
-                print(f"******\ncarpan {carpan} ye yukseldi!\n******")
-                continue
+    return text, para, rebirth, artis, carpan, fiyat2, stok1, stok2
 
-            elif stok1 == 1:
-                print("******\nBu esyanin stogu bitti!\n******")
-                continue
+def prestige(para, rebirth, prestige, artis, carpan1, carpan2, fiyat3, stok2):
+    if para >= fiyat3 and stok2 == 1:
+        para = 0
+        rebirth = 0
+        prestige += 1
+        artis = 10
+        carpan1 = 1
+        carpan2 *= 2
+        fiyat3 = int(fiyat3 * 5)
+        stok2 = 25
 
-            else:
-                print("******\nPara yetersiz!\n******")
-                continue
+        text = f"******\nprestige {prestige} ye yukseldi!\n******"
 
-        elif secim == 3:
-            print("Marketten cikiliyor...")
+    elif para < fiyat3:
+        text = "******\nPara yetersiz!\n******"
 
-        else:
-            print("Gecersiz secim!")
+    elif stok2 > 1:
+        text = "Rebirth fullenmeden Prestige satın alamazsın!"
 
-        return para, artis, carpan, fiyat1, fiyat2, stok1, stok2
+    return text, para, rebirth, prestige, artis, carpan1, carpan2, fiyat3, stok2
 
-def tikla(para, artis, carpan):
-    para += artis*carpan
+def tikla(para, artis, carpan1, carpan2):
+    para += artis*carpan1*carpan2
     return int(para)
-
-def basla(kullaniciAdi, sifre, para, artis, carpan, fiyat1, fiyat2, stok1, stok2):
-    while True:
-        print(f"para = {para}              /{kullaniciAdi}")
-        secim = int(input(" 1-Tikla\n 2-Market\n 3-Cikis\n>-<\n "))
-        print(">-<")
-        oyuncuGuncelle(kullaniciAdi, sifre, para, artis, carpan, fiyat1, fiyat2, stok1, stok2)
-        if secim==1:
-            para = tikla(para, artis, carpan)
-
-        elif secim==2:
-            para, artis, carpan, fiyat1, fiyat2, stok1, stok2 = market(para, artis, carpan, fiyat1, fiyat2, stok1, stok2)
-
-        elif secim==3:
-            print("Hesaptan cikiliyor...")
-            break
-        else:
-            print("Gecersiz secim.")
--- main.py
+++ main.py
@@ -13,6 +13,10 @@ class p(QMainWindow):
         self.p.setupUi(self)
         self.p1 = p1()
         self.p2 = p2()
+        self.p3 = p3()
+        self.p4 = p4()
+        self.p1.set_p3(self.p3)
+        self.p4.set_p3(self.p3)
         self.p.pushButton.clicked.connect(self.buton)
         self.p.pushButton_2.clicked.connect(self.buton2)
         self.p.pushButton_3.clicked.connect(self.buton3)
@@ -33,20 +37,28 @@ class p1(QMainWindow):
         super().__init__()
         self.p1 = panel1.Ui_MainWindow()
         self.p1.setupUi(self)
-        self.p3 = p3()
+        self.p3 = None
         self.p1.pushButton.clicked.connect(self.buton)
         self.p1.pushButton_2.clicked.connect(self.anaEkran)
         self.deneme = 1
 
+    def set_p3(self, p3_instance):
+        self.p3 = p3_instance
+
     def buton(self):
         self.kullaniciAdi = self.p1.lineEdit.text()
         self.sifre = self.p1.lineEdit_2.text()
-        girisDurum, self.kullaniciAdi, self.sifre, self.para, self.artis, self.carpan, self.fiyat1, self.fiyat2, self.stok1, self.stok2 = fonksiyonlar.giris(self.kullaniciAdi, self.sifre)
+        (girisDurum, self.kullaniciAdi, self.sifre, self.para, self.rebirth, self.prestige, self.artis, self.carpan1,
+         self.carpan2, self.fiyat1, self.fiyat2, self.fiyat3, self.stok1, self.stok2) = fonksiyonlar.giris(self.kullaniciAdi, self.sifre)
+
         if girisDurum == 0:
             self.p1.label_3.setText(f"Kullanici adi ve/veya sifre yanlis. <{self.deneme}. deneme>")
             self.deneme += 1
 
         else:
+            if self.p3:
+                self.p3.setData(self.kullaniciAdi, self.sifre, self.para, self.rebirth, self.prestige, self.artis,
+                            self.carpan1, self.carpan2, self.fiyat1, self.fiyat2, self.fiyat3, self.stok1, self.stok2)
             self.close()
             self.p3.show()
 
@@ -85,20 +97,42 @@ class p3(QMainWindow):
         super().__init__()
         self.p3 = panel3.Ui_MainWindow()
         self.p3.setupUi(self)
-        self.p4 = p4()
         self.p3.pushButton.clicked.connect(self.tikla)
         self.p3.pushButton_2.clicked.connect(self.market)
         self.p3.pushButton_3.clicked.connect(self.anaEkran)
 
+    def setData(self, kullaniciAdi, sifre, para, rebirth, prestige, artis, carpan1, carpan2, fiyat1, fiyat2, fiyat3, stok1, stok2):
+        self.kullaniciAdi = kullaniciAdi
+        self.sifre = sifre
+        self.para = para
+        self.rebirth = rebirth
+        self.prestige = prestige
+        self.artis = artis
+        self.carpan1 = carpan1
+        self.carpan2 = carpan2
+        self.fiyat1 = fiyat1
+        self.fiyat2 = fiyat2
+        self.fiyat3 = fiyat3
+        self.stok1 = stok1
+        self.stok2 = stok2
+
+        self.p3.label.setText(f"Para = {self.para}")
+        self.p3.label_2.setText(f"/{self.kullaniciAdi}")
+        self.p3.label_3.setText(f"Rebirth = {self.rebirth}")
+        self.p3.label_4.setText(f"Prestige = {self.prestige}")
+        self.p3.label_5.setText(f"{self.carpan1*self.carpan2}x Carpan")
+
     def tikla(self):
-        self.close()
-        self.p = p()
-        self.p.show()
+        self.para = fonksiyonlar.tikla(self.para, self.artis, self.carpan1, self.carpan2)
+        self.p3.label.setText(f"Para = {self.para}")
 
     def market(self):
+        self.p4 = p4()
+        self.p4.setData(self.kullaniciAdi, self.sifre, self.para, self.rebirth, self.prestige, self.artis,
+                        self.carpan1, self.carpan2, self.fiyat1, self.fiyat2, self.fiyat3, self.stok1, self.stok2)
+        self.p4.set_p3(self)
         self.close()
-        self.p = p()
-        self.p.show()
+        self.p4.show()
 
     def anaEkran(self):
         self.close()
@@ -110,10 +144,32 @@ class p4(QMainWindow):
         super().__init__()
         self.p4 = panel4.Ui_MainWindow()
         self.p4.setupUi(self)
+        self.p3 = None
         self.p4.pushButton.clicked.connect(self.buton)
         self.p4.pushButton_2.clicked.connect(self.buton2)
         self.p4.pushButton_3.clicked.connect(self.buton3)
-        self.p4.pushButton_4.clicked.connect(self.anaEkran)
+        self.p4.pushButton_4.clicked.connect(self.geri)
+
+    def set_p3(self, p3_instance):
+        self.p3 = p3_instance
+
+    def setData(self, kullaniciAdi, sifre, para, rebirth, prestige, artis, carpan1, carpan2, fiyat1, fiyat2, fiyat3, stok1, stok2):
+        self.kullaniciAdi = kullaniciAdi
+        self.sifre = sifre
+        self.para = para
+        self.rebirth = rebirth
+        self.prestige = prestige
+        self.artis = artis
+        self.carpan1 = carpan1
+        self.carpan2 = carpan2
+        self.fiyat1 = fiyat1
+        self.fiyat2 = fiyat2
+        self.fiyat3 = fiyat3
+        self.stok1 = stok1
+        self.stok2 = stok2
+
+        self.p4.label.setText(f"Para = {self.para}")
+        self.p4.label_2.setText(f"/{kullaniciAdi}")
 
     def buton(self):
         self.close()
@@ -130,10 +186,14 @@ class p4(QMainWindow):
         self.p = p()
         self.p.show()
 
-    def anaEkran(self):
+    def geri(self):
+        if self.p3:
+            self.p3.setData(self.kullaniciAdi, self.sifre, self.para, self.rebirth, self.prestige, self.artis,
+                            self.carpan1, self.carpan2, self.fiyat1, self.fiyat2, self.fiyat3, self.stok1, self.stok2)
+        fonksiyonlar.oyuncuGuncelle(self.kullaniciAdi, self.sifre, self.para, self.rebirth, self.prestige, self.artis,
+                            self.carpan1, self.carpan2, self.fiyat1, self.fiyat2, self.fiyat3, self.stok1, self.stok2)
         self.close()
-        self.p = p()
-        self.p.show()
+        self.p3.show()
 
 
 app = QApplication([])
